53fedf0cc956f47eb8f29facac667b83ce616e95
gunhee0421 <rjsgml771@naver.com>
2023-07-20 21:52:50 +0900

    Add
    base spring

---
+++ Novel_Test/src/main/java/Novel_Test/Novel_Test/NovelTestApplication.java
@@ -0,0 +1,13 @@
+package Novel_Test.Novel_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelTestApplication.class, args);
+	}
+
+}
---
+++ Novel_Test/src/test/java/Novel_Test/Novel_Test/NovelTestApplicationTests.java
@@ -0,0 +1,13 @@
+package Novel_Test.Novel_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
